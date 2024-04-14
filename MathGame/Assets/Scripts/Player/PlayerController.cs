5b2779ecef07ac73951e20fb5e17db9c4914e46e
Dilon Fernando <149017370+DilonFernando03@users.noreply.github.com>
2024-04-14 20:31:30 +0100

    improved movement

--- MathGame/Assets/Scripts/Player/PlayerController.cs
+++ MathGame/Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+<<<<<<< Updated upstream
     [SerializeField] public float speed = 5f;
     [SerializeField] private float y_min, y_max;
     [SerializeField] private float x_min, x_max;
     
   
+=======
+    [SerializeField] public float speed = 5f; //Remove when implementing vertices type movement.
+    [SerializeField] private float shooting_Timer = 0.5f;
+    [SerializeField] private GameObject shooting_Position;
+    [SerializeField] private GameObject shooting_Bullets;
+
+
+
+    private float current_Shooting_Timer;
+    private bool canShoot;
+
+    //Those 2 Are boundries for X AND Y position
+    private int maxXpos = 6;
+    private int maxYpos = 6;
+                                
+
+
+    private void Start()
+    {
+        current_Shooting_Timer = shooting_Timer;
+    }
+
+>>>>>>> Stashed changes
     void Update()
     {
         PlayerMovement();
@@ -17,6 +41,7 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
+<<<<<<< Updated upstream
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -38,4 +63,57 @@ public class PlayerController : MonoBehaviour
     }
     
     
+=======
+
+        //Vertical Movement
+        if (Input.GetKeyDown("up") && transform.position.y < maxYpos)
+        {
+            Vector3 temp = transform.position;
+            temp.y += 1f;
+            transform.position = temp;
+        }
+        else if (Input.GetKeyDown("down") && transform.position.y > -maxYpos)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= 1f;
+            transform.position = temp;
+        }
+        
+        //Horizontal Movement
+        else if (Input.GetKeyDown("right") && transform.position.x < maxXpos)
+        {
+            Vector3 temp = transform.position;
+            temp.x += 1f;
+            transform.position = temp;
+        }
+        else if (Input.GetKeyDown("left") && transform.position.x > -maxXpos)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= 1f;
+            transform.position = temp;
+        }
+    }
+
+    void Shoot()
+    {
+        shooting_Timer += Time.deltaTime;
+        if (shooting_Timer > current_Shooting_Timer)
+        {
+            canShoot = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canShoot)
+            {
+                canShoot = false;
+                shooting_Timer = 0f;
+            
+                Instantiate(shooting_Bullets, shooting_Position.transform.position, Quaternion.identity);
+            
+                //play sound effects
+            }
+        }
+    } 
+>>>>>>> Stashed changes
 }
